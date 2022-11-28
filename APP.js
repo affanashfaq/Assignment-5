933df7998f1a85525099c51b79e8ace39eb96bdc
unknown <affanashfaq786@gmail.com>
2022-11-28 02:52:19 -0800

    work publish

---
+++ APP.js
@@ -0,0 +1,78 @@
+var abc = 'Welcome To This Page'
+alert(abc)
+
+
+var textinput = document.getElementById('inp')
+var errorinput = document.getElementById('error')
+var a =[];
+
+function addvalue(){
+    var b = textinput.value;
+    if(b == ""){
+        errorinput.value = "Enter Value";
+    }else{
+    a.push(b);
+    console.Log(a);
+    }
+}
+
+var rightinput = document.getElementById('leo')
+var wronginput = document.getElementById('wrong')
+var b = [];
+function addnumber(){
+    var c = rightinput.value;
+    if(c == ""){
+        errorinput.value = "Enter number"
+    }else{
+        a.push(c);
+        console.log(b);
+    }
+}
+var rightinput = document.getElementById('leo')
+var wronginput = document.getElementById('wrong')
+var b = [];
+function addstrike(){
+    var c = rightinput.value;
+    if(c == ""){
+        errorinput.value = "Enter email"
+    }else{
+        a.push(c);
+        console.log(b);
+    }
+}
+var rightinput = document.getElementById('leo')
+var wronginput = document.getElementById('wrong')
+var b = [];
+function addwork(){
+    var c = rightinput.value;
+    if(c == ""){
+        errorinput.value = "Enter password"
+    }else{
+        a.push(c);
+        console.log(b);
+    }
+}
+var rightinput = document.getElementById('leo')
+var wronginput = document.getElementById('wrong')
+var b = [];
+function addcomplete(){
+    var c = rightinput.value;
+    if(c == ""){
+        errorinput.value = "Enter Number"
+    }else{
+        a.push(c);
+        console.log(b);
+    }
+}
+var rightinput = document.getElementById('leo')
+var wronginput = document.getElementById('wrong')
+var b = [];
+function addpublish(){
+    var c = rightinput.value;
+    if(c == ""){
+        errorinput.value = "Enter CNIC"
+    }else{
+        a.push(c);
+        console.log(b);
+    }
+}
